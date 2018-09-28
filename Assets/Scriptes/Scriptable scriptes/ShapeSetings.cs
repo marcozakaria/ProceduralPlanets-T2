f67a74929a36bdfedb358786f04047b6367d206f
Marco zakaria <marcozakaria@yahoo.com>
2018-09-28 23:12:06 +0200

    Adding mask from first layer

--- Assets/Scriptes/Scriptable scriptes/ShapeSetings.cs
+++ Assets/Scriptes/Scriptable scriptes/ShapeSetings.cs
@@ -8,10 +8,11 @@ public class ShapeSetings : ScriptableObject
     public float planetRadius = 1f;
     public NoiseLayer[] noiseLayers;
 
-    [System.Serializable] // to show up in the inspector
-    public class NoiseLayer  // to have multiple noise settings and layer them on top of each other( to make shape o contents and mountains)
+    [System.Serializable]    // to show up in the inspector
+    public class NoiseLayer   // to have multiple noise settings and layer them on top of each other( to make shape o contents and mountains)
     {
-        public bool enabled = true; // to toogle visability os single noise  layer
+        public bool enabled = true;     // to toogle visability os single noise  layer
+        public bool useFirstLayerAsMask = false;    // to avoide mountane apperiance alone inside the sea
         public NoiseSettings noiseSettings;
     }
 }
--- Assets/Scriptes/ShapeGenerator.cs
+++ Assets/Scriptes/ShapeGenerator.cs
@@ -20,12 +20,24 @@ public class ShapeGenerator
     public Vector3 PointOnUnitPlanet(Vector3 pointOnUnitSphere)
     {
         //float elevation = noiseFilter.Evaluate(pointOnUnitSphere);
+        float firstLayerValue=0;
         float elevation = 0;
-        for (int i = 0; i < noiseFilters.Length; i++)
+
+        if (noiseFilters.Length >0)
+        {
+            firstLayerValue = noiseFilters[0].Evaluate(pointOnUnitSphere);
+            if (shapeSetings.noiseLayers[0].enabled)  // to have ability to enable and disable the layers
+            {
+                elevation += firstLayerValue; // to add first layer to be used as a mask for next layers 
+            }
+        }
+
+        for (int i = 1; i < noiseFilters.Length; i++)
         {
             if (shapeSetings.noiseLayers[i].enabled)  // to have ability to enable and disable the layers
             {
-                elevation += noiseFilters[i].Evaluate(pointOnUnitSphere); // to add noise layers to gether  
+                float mask = (shapeSetings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 1;  // if we checked to use first layer as a mask
+                elevation += noiseFilters[i].Evaluate(pointOnUnitSphere) *mask; // to add noise layers to gether  
             }
         }
         return pointOnUnitSphere * shapeSetings.planetRadius *(1+elevation); // (elevation +1) bec, planet point must be not = to zero
