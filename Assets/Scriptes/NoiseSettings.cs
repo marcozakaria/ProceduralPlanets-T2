1e4a8794fa8c2d18dbe09d7a3859550d1706f009
Marco zakaria <marcozakaria@yahoo.com>
2018-10-02 11:53:52 +0200

    Conditional hide attribute
    show settings of selected filter only

--- Assets/Scriptes/NoiseSettings.cs
+++ Assets/Scriptes/NoiseSettings.cs
@@ -8,7 +8,9 @@ public class NoiseSettings
     public enum FilterType { Simple , Ridged };
     public FilterType filterType;
 
+    [ConditionalHide("filterType",0)]
     public SimpleNoiseSettings simpleNoiseSettings;
+    [ConditionalHide("filterType", 1)]
     public RidgedNoiseSettings ridgedNoiseSettings;
 
     [System.Serializable]
